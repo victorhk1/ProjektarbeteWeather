ab55feae335d526228fd5721baf963c82d4f9994
victorhk1 <44748130+victorhk1@users.noreply.github.com>
2018-11-05 01:28:03 +0100

    Update date-add.pipe.spec.ts

--- src/app/date-add.pipe.spec.ts
+++ src/app/date-add.pipe.spec.ts
@@ -6,3 +6,16 @@ describe('DateAddPipe', () => {
     expect(pipe).toBeTruthy();
   });
 });
+
+describe('DateAddPipe', () => {
+  // This pipe is a pure, stateless function so no need for BeforeEach
+  let pipe = new DateAddPipe();
+ //simple example that checks if date, time works
+  it('transforms "2018-11-06 & 07:00:00" to "2018-11-06 & 08:00:00"', () => {
+    expect(pipe.transform('2018-11-06 & 07:00:00')).toBe('2018-11-06 & 08:00:00');
+  });
+ 
+ 
+
+
+});
